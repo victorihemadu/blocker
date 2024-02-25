904dc1e7ff0debdb301a82dc487732f4c1d102ab
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2024-02-25 10:06:24 +0100

    created a test file

--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -1,1 +1,13 @@
 package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePrivateKey(t *testing.T) {
+	privKey := generatePrivateKey()
+
+	assert.Equal(t, len(privKey.Bytes()), privKeyLen)
+}
